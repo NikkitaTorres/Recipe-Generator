e35dab8951b2f1ebb539ea9c5b2e0f085612e15d
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-04-08 13:14:33 -0700

    pushing new files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Home from './Components/Home';
+import Footer from './Components/Footer';
 
 
 
@@ -10,6 +11,7 @@ root.render(
   <React.StrictMode>
     <App />
     <Home />
+    <Footer />
   </React.StrictMode>
 );
 
