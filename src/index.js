90a1a7f6dc60da89e947d4365da8d40976d205cb
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-04-08 11:59:09 -0700

    Initial Commit
    Co-authored-by: Zuri Gallegos
    <Zurisadailopezmartinez@gmail.com>, Ravin ravesterrr@gmail.com, Monica monboza@gmail.com, Nikkita nikkitatorres@yahoo.com

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Home from './Components/Home';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <Home />
+  </React.StrictMode>
+);
+
