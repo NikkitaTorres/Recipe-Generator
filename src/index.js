838fbda1ec1835cb76c6d36d27a535b60df98079
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-04-10 10:48:45 -0700

    add updated server.js, app.js, header.js + footer.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import App from './App';
 import Footer from './Components/Footer';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
     <Header />
     <Home />
+    <App />
     <Footer />
   </React.StrictMode>
 );
