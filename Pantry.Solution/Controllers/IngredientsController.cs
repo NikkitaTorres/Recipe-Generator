1a1e22051e6329d5f4b11138b2ab68bd45958a86
NikkitaTorres <nikkitatorres@yahoo.com>
2024-04-10 15:51:27 -0700

    update Ingredients controller to remove SetIngredient and PC update

--- Pantry.Solution/Controllers/IngredientsController.cs
+++ Pantry.Solution/Controllers/IngredientsController.cs
@@ -1,65 +1,69 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Pantry.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace Pantry.Controllers;
+namespace Pantry.Controllers
+{
 
 public class IngredientsController : Controller
 {
-  private readonly PantryContext _db;
-  public IngredientsController(PantryContext db)
-  {
-    _db = db;
-  }
- public ActionResult Index()
-  {
-    List<Ingredient> model = _db.Ingredients
-    .Include(i => i.IngredientRecipes)
-    .ToList();
-    return View(model);
-  }
+private readonly PantryContext _db;
 
-   public ActionResult Create()
-  {
-    return View(SetIngredient(new Ingredient(), "Create"));
-  }
+        public IngredientsController(PantryContext db)
+        {
+            _db = db;
+        }
 
-  [HttpPost]
-  public ActionResult Create(Ingredient ingredient)
-  {
-    if(!ModelState.IsValid)
-    {
-      return View(SetIngredient(ingredient, "Create"));
-    }
-    _db.Ingredients.Add(ingredient);
-    _db.SaveChanges();
-    return RedirectToAction("Details", new { id = ingredient.IngredientId});
-  }
-  public ActionResult Edit(int id)
-  {
-    Ingredient target = _db.Ingredients.FirstOrDefault(i => i.IngredientId == id);
-    return View(SetIngredient(target, "Edit"));
-  }
-  [HttpPost]
-  public ActionResult Edit(Ingredient ingredient)
-  {
-    if (!ModelState.IsValid)
-    {
-      return View(SetIngredient(ingredient, "Edit"));
-    }
-    _db.Ingredients.Update(ingredient);
-    _db.SaveChanges();
-    return RedirectToAction("Details", new { id = ingredient.IngredientId});
-  }
+        public ActionResult Index()
+        {
+            List<Ingredient> model = _db.Ingredients
+                .Include(i => i.IngredientRecipes)
+                .ToList();
+            return View(model);
+        }
 
-  public ActionResult Details(int id)
-  {
-    Ingredient ingredient = _db.Ingredients
-    .Include(i => i.IngredientRecipes)
-    .ThenInclude(iR => iR.Recipe)
-    .FirstOrDefault(i => i.IngredientId == id);
-    return View(ingredient);
-  }
+        public ActionResult Create()
+        {
+            return View(new Ingredient()); // Removed SetIngredient method call
+        }
+
+        [HttpPost]
+        public ActionResult Create(Ingredient ingredient)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ingredient); // Removed SetIngredient method call
+            }
+            _db.Ingredients.Add(ingredient);
+            _db.SaveChanges();
+            return RedirectToAction("Details", new { id = ingredient.IngredientId });
+        }
+  // public ActionResult Edit(int id)
+  // {
+  //   Ingredient target = _db.Ingredients.FirstOrDefault(i => i.IngredientId == id);
+  //   //return View(SetIngredient(target, "Edit"));
+  // }
+  // [HttpPost]
+  // public ActionResult Edit(Ingredient ingredient)
+  // {
+  //   if (!ModelState.IsValid)
+  //   {
+  //     return View(SetIngredient(ingredient, "Edit"));
+  //   }
+  //   _db.Ingredients.Update(ingredient);
+  //   _db.SaveChanges();
+  //   return RedirectToAction("Details", new { id = ingredient.IngredientId});
+  // }
+
+  // public ActionResult Details(int id)
+  // {
+  //   Ingredient ingredient = _db.Ingredients
+  //   .Include(i => i.IngredientRecipes)
+  //   .ThenInclude(iR => iR.Recipe)
+  //   .FirstOrDefault(i => i.IngredientId == id);
+  //   return View(ingredient);
+  // }
 
   [HttpPost]
   public ActionResult Delete(int id)
@@ -76,4 +80,5 @@ public class IngredientsController : Controller
       return RedirectToAction("Details", new { id = target.IngredientId});
     }
   }
+}
 }
--- Pantry.Solution/Models/PantryContext.cs
+++ Pantry.Solution/Models/PantryContext.cs
@@ -2,19 +2,16 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-
-
-
-
 namespace Pantry.Models
 {
+    public class PantryContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<IngredientRecipe> IngredientRecipes { get; set; }
 
-
-public class PantryContext 
-{
-  public DbSet<Recipe> Recipes { get; set; }
-  public DbSet<Ingredient> Ingredients { get; set; }
-  public DbSet<IngredientRecipe> IngredientRecipes { get; set; }
-  // public PantryContext(DbContextOptions options) : base(options) { }
-}
+        public PantryContext(DbContextOptions<PantryContext> options) : base(options)
+        {
+        }
+    }
 }
