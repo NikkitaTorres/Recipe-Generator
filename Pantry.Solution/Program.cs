f589efaf88d1a717f3a6e18df3875d4d6902f5c6
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-04-10 10:51:46 -0700

    updated ingredients controller

---
+++ Pantry.Solution/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Pantry.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace Pantry
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddControllersWithViews();
+
+      WebApplication app = builder.Build();
+
+      // app.UseDeveloperExceptionPage();
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+
+      app.Run();
+    }
+  }
+}
