32951574f4cc71f359dd9ec292853f54987d1253
NikkitaTorres <147372608+NikkitaTorres@users.noreply.github.com>
2024-04-11 16:48:55 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Components/Home';
-import App from './App';
 import Footer from './Components/Footer';
 import UserProfile from './Components/UserProfile';
-
+import SignIn from './Components/SignIn';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <Header />
-    <Home />
-    {/* <App /> */}
-    <UserProfile />
+    <React.StrictMode>
+      <Routes>
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/Home" element={<Home />} />
+      </Routes>
+      <UserProfile />
+    </React.StrictMode>
     <Footer />
-  </React.StrictMode>
+  </Router>
 );
