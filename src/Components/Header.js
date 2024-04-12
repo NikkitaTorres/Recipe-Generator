9b1e4a24f773416a2e09a07e273ef4ba59e13008
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-04-12 12:41:24 -0700

    add validation for sign in users updated header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import SignIn from './SignIn';
 import '../Styles/Header.css';
 import Logo from '../assets/Images/Rec.png'
 
 
 function Header() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const handleSignIn = () => {
+    window.location.href = '/signin';
+  }
+
+  const handleSignOut = () => {
+    setIsSignedIn(false);
+  }
 
 
   return (
@@ -12,8 +23,19 @@ function Header() {
         <img src={Logo} alt='Recipe Generator logo' className='logo' />
       </div>
       <div className='menu'>
-        <button className="bg-purple-400 hover:bg-purple-300 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >Home</button>
-        <button className="bg-purple-400 hover:bg-purple-300 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >Pantry</button>
+      <Link to="/">
+          <button className="bg-purple-400 hover:bg-purple-300 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >Home</button>
+        </Link>
+        {isSignedIn ? (
+          <>
+          <Link to="/pantry">
+            <button className="bg-purple-400 hover:bg-purple-300 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >Pantry</button>
+          </Link>
+          <button onClick={handleSignOut} className="bg-purple-400 hover:bg-purple-300 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >Sign Out</button>
+        </>
+      ) : (
+        <button onClick={handleSignIn} className="bg-purple-400 hover:bg-purple-300 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" >Sign In</button>
+      )}
       </div>
     </header>
   );
